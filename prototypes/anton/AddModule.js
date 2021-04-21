02b7b329ab18d951c044693c865ba22e43a5a23c
antoroesler <anton.roesler98@gmx.de>
2021-04-21 18:38:00 +0200

    Some ideas for an prototype

--- prototypes/anton/AddModule.js
+++ prototypes/anton/AddModule.js
@@ -1,17 +1,135 @@
+var $ = go.GraphObject.make;
+
+
+function readAppName() {
+  var textField = document.getElementById("a-name");
+  appName = textField.value;
+  console.log(appName)
+  D.add(appName, "lightblue")
+}
+
+
+class ApplicationDiagramm {
+  constructor() {
+    this.diagram = $(go.Diagram, "dia2");
+    this.nodeDataArray = [];
+    this.linkDataArray = [];
+    this.diagram.model = new go.GraphLinksModel(this.nodeDataArray, this.linkDataArray);
+    this.diagram.nodeTemplate =
+      $(go.Node, "Auto",
+        $(go.Shape, "RoundedRectangle", {
+            fill: "white"
+          },
+          new go.Binding("fill", "color")),
+        $(go.TextBlock, "Text", {
+            margin: 10
+          },
+          new go.Binding("text", "key"))
+      );
+  }
+
+  add(name, color) {
+    this.nodeDataArray.push({
+      key: name,
+      color: color
+    })
+    this.diagram.startTransaction();
+    this.diagram.updateAllRelationshipsFromData();
+    this.diagram.updateAllTargetBindings();
+    this.diagram.commitTransaction("update");
+  }
+}
+
+var D = new ApplicationDiagramm();
+
+function createApp(name) {
+  sd = $(go.Diagram, "dia2");
+  var nodeDataArray = [];
+  var linkDataArray = [];
+  sd.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
+
+  sd.nodeTemplate =
+    $(go.Node, "Auto",
+      $(go.Shape, "RoundedRectangle", {
+          fill: "white"
+        },
+        new go.Binding("fill", "color")),
+      $(go.TextBlock, "Text", {
+          margin: 10
+        },
+        new go.Binding("text", "key"))
+    );
+}
+
 function init() {
-  var $ = go.GraphObject.make;
   myDiagram = $(go.Diagram, "myDia");
   var nodeDataArray = [{
-      key: "Peter"
+      key: "Peter",
+      color: "lime"
     },
     {
-      key: "Fridel"
+      key: "Fridel",
+      color: "cyan"
+    },
+    {
+      key: "Die Gang",
+      isGroup: true
+    },
+    {
+      key: "Günni",
+      color: "pink",
+      group: "Die Gang"
+    },
+    {
+      key: "Alfred",
+      color: "green",
+      group: "Die Gang"
     }
+
   ];
   var linkDataArray = [{
-    to: "Peter",
-    from: "Fridel"
-  }];
+      to: "Peter",
+      from: "Fridel",
+      color: "red"
+    },
+    {
+      from: "Günni",
+      to: "Alfred",
+      color: "black"
+    },
+    {
+      from: "Fridel",
+      to: "Alfred",
+      color: "Red"
+    }
+
+  ];
   myDiagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
-  console.log("Done")
+
+  myDiagram.nodeTemplate =
+    $(go.Node, "Auto",
+      $(go.Shape, "RoundedRectangle", {
+          fill: "white"
+        },
+        new go.Binding("fill", "color")),
+      $(go.TextBlock, "Text", {
+          margin: 10
+        },
+        new go.Binding("text", "key"))
+    );
+
+  myDiagram.linkTemplate =
+    $(go.Link,
+      $(go.Shape, {
+          strokeWidth: 3
+        },
+        new go.Binding("stroke", "color")
+      ),
+      $(go.Shape, {
+          toArrow: "Standard",
+          stroke: null
+        },
+        new go.Binding("fill", "color"))
+    );
+  myDiagram.layout = $(go.CircularLayout);
 }
