6b35821daaf6c44d915394bcd6b12ed856704702
antoroesler <anton.roesler98@gmx.de>
2021-04-29 17:29:24 +0200

    initial file structure

---
+++ index.js
@@ -0,0 +1,37 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ *  App Configuration
+ */
+
+app.use(express.static('public')) // set the static files location
+
+/**
+ * Routes Definitions
+ */
+
+app.get("/", (req, res) => {
+  res.sendFile("./public/index.html");
+});
+
+/**
+ * Server Activation
+ */
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
