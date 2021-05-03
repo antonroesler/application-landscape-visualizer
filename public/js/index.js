f8453190ed2c66b98ec28cd133e3616ee640cda4
fengyilu <fengyilu1998@gmail.com>
2021-05-03 20:27:51 +0200

    added header.txt

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,20 @@
+/*
+* Copyright (c) 2021 Ecore. All rights reserved.
+*
+* University:		 Frankfurt University of Applied Sciences
+* Study program:	 Engineering Business Information Systems
+* Module:		   Advanced Programming 2021
+* Professor:		   Prof. Dr. Jung, Prof. Dr. Bremm
+* Date:			     21.04.2021
+*
+*/
+
+/**
+* A short description what this file/class is all about.
+* @author Leonard Hußke , Feng Yi Lu
+* this file contains the init() function that is needed to display the Diagram Canvas in the HTML File 
+*/
+
 const $ = go.GraphObject.make;
 const diagram = $(go.Diagram, "diagramDiv",
     { // enable Ctrl-Z to undo and Ctrl-Y to redo
@@ -8,53 +25,10 @@ model.nodeDataArray = [{ key: "Component", category: "Component", desc: "metadat
 model.linkDataArray = [];
 
 
-var databaseTemplate =
-    $(go.Node, "Vertical",  // "vertical" determine the order of the go.objects 
-        $(go.Shape, "Database", { height: 70, width: 70, fill: "blue" }, //per default color and size is setted, can be customised through e.g. "go.Binding()"
-            new go.Binding("fill", "color")),
-        $(go.TextBlock, { font: "bold 12pt sans-serif" },
-            new go.Binding("text", "key")),
-        $(go.TextBlock, "Description:"),
-        $(go.TextBlock, new go.Binding("text", "desc")),
-
-    );
-
-var componentTemplate =
-    $(go.Node, "Vertical",
-        $(go.Shape, "Component", { height: 70, width: 70, fill: "red" },
-            new go.Binding("fill", "color")),
-
-        $(go.TextBlock, { font: "bold 12pt sans-serif" },
-            new go.Binding("text", "key")),
-        $(go.TextBlock, "Description:"),
-        $(go.TextBlock, new go.Binding("text", "desc")),
-
-    );
-
-var packageTemplate =
-    $(go.Node, "Vertical",
-        $(go.Shape, "Package", { height: 70, width: 70, fill: "green" },
-            new go.Binding("fill", "color")),
-
-        $(go.TextBlock, { font: "bold 12pt sans-serif" },
-            new go.Binding("text", "key")),
-        $(go.TextBlock, "Description:"),
-        $(go.TextBlock, new go.Binding("text", "desc")),
-
-    );
-
-// templmap includes all of our templates, we can get access to with "category" inside of an NodeObject. Name of the template is defined in templmap.add("name", variable of Template)
-var templmap = new go.Map();
-templmap.add("", diagram.nodeTemplate);
-templmap.add("Database", databaseTemplate);
-templmap.add("Component", componentTemplate)
-templmap.add("Package", packageTemplate);
-
-
 function init() {
 
     diagram.model = model;
-    // passing our TemplateMap into our diagramS
+    // passing our TemplateMap into our diagram
     diagram.nodeTemplateMap = templmap;
 
     diagram.layout = $(go.LayeredDigraphLayout);
