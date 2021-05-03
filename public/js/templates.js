bc7d2052c815298c4c2066fe4257b835adc1b5e4
fengyilu <fengyilu1998@gmail.com>
2021-05-03 21:15:33 +0200

    comment update

--- public/js/templates.js
+++ public/js/templates.js
@@ -10,26 +10,35 @@
 */
 
 /**
-* A short description what this file/class is all about.
 * @author Feng Yi Lu
 *this file contains all templates that will be used for different Nodes
-*- templates are used for various Nodes, its a configuration for a certain type of Node e.g. Database
+*- templates are used for various Nodes, its a configuration for a certain type
+   of Node e.g. Database
 *- usage: in this file we create a variable that contains a preset of a Node-style
 *-        these variables can be used in the index.js file. 
-*-        "templmap" is a Map that contains all of our templates that we create, with the function add("category-name", template-variable) we can add more templates to it. 
-*-        In Index.js we declare a TemplateMap to our diagram "diagram.nodeTemplateMap = templmap;" with that our nodes are able to use these templates (the attribute "category" inside of the Node Object declares which template it uses)
+*-        "templmap" is a Map that contains all of our templates that we create, with 
+          the function add("category-name", template-variable) we can add more templates to it. 
+*-        In Index.js we declare a TemplateMap to our diagram "diagram.nodeTemplateMap = templmap;"
+          with that our nodes are able to use these templates 
+         (the attribute "category" inside of the Node Object declares which template it uses)
 *        
 */
 
 var databaseTemplate =
-    $(go.Node, "Vertical",  // "vertical" determine the order of the go.objects 
-        $(go.Shape, "Database", { height: 70, width: 70, fill: "blue" }, //per default color and size is setted, can be customised through e.g. "go.Binding()"
+    // "vertical" determine the order of the go.objects 
+    $(go.Node, "Vertical",
+        //per default color and size is setted, can be customised through e.g. "go.Binding()"
+        $(go.Shape, "Database", { height: 70, width: 70, fill: "blue" },
             new go.Binding("fill", "color")),
         $(go.TextBlock, { font: "bold 12pt sans-serif" },
             new go.Binding("text", "key")),
         $(go.TextBlock, "Description:"),
         $(go.TextBlock, new go.Binding("text", "desc")),
     );
+/**
+* databaseTemplate
+* is used for generating a "database" node
+*/
 
 var componentTemplate =
     $(go.Node, "Vertical",
@@ -41,6 +50,10 @@ var componentTemplate =
         $(go.TextBlock, "Description:"),
         $(go.TextBlock, new go.Binding("text", "desc")),
     );
+/**
+* componentTemplate
+* is used for generating a "component (part of an Application-node)" node 
+*/
 
 var packageTemplate =
     $(go.Node, "Vertical",
@@ -52,8 +65,14 @@ var packageTemplate =
         $(go.TextBlock, "Description:"),
         $(go.TextBlock, new go.Binding("text", "desc")),
     );
+/**
+* packageTemplate
+* is used for generating a "Application" node 
+*/
 
-// templmap includes all of our templates, we can get access to with "category" inside of an NodeObject. Name of the template is defined in templmap.add("name", variable of Template)
+
+/**templmap includes all of our templates, we can get access to with "category" inside of an NodeObject. 
+Name of the template is defined in templmap.add("name", variable of Template)*/
 var templmap = new go.Map();
 templmap.add("", diagram.nodeTemplate);
 templmap.add("Database", databaseTemplate);
