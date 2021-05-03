703666eb51c5fcc1b35a764b756279ec5d66dc0a
fengyilu <fengyilu1998@gmail.com>
2021-05-03 17:48:10 +0200

    Adding 3 base Templates

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,63 @@
 const $ = go.GraphObject.make;
-
 const diagram = $(go.Diagram, "diagramDiv",
     { // enable Ctrl-Z to undo and Ctrl-Y to redo
         "undoManager.isEnabled": true
     });
-
 const model = $(go.GraphLinksModel);
-
-model.nodeDataArray = [ { key:"1", color:"blue" } ];
+model.nodeDataArray = [{ key: "Component", category: "Component", desc: "metadata" }, { key: "Neo4J", color: "white", category: "Database", desc: "metadata" }, { key: "Application", category: "Package", desc: "metadata" }];
 model.linkDataArray = [];
 
-const defaultTemplate = $(go.Node, "Auto",
-        $(go.Shape, "RoundedRectangle", {
-            fill: "white"
-        },
-        new go.Binding("fill", "color")),
-        $(go.TextBlock, "Text", {
-            margin: 10
-        },
-        new go.Binding("text", "key"))
-);
+
+var databaseTemplate =
+    $(go.Node, "Vertical",  // "vertical" determine the order of the go.objects 
+        $(go.Shape, "Database", { height: 70, width: 70, fill: "blue" }, //per default color and size is setted, can be customised through e.g. "go.Binding()"
+            new go.Binding("fill", "color")),
+        $(go.TextBlock, { font: "bold 12pt sans-serif" },
+            new go.Binding("text", "key")),
+        $(go.TextBlock, "Description:"),
+        $(go.TextBlock, new go.Binding("text", "desc")),
+
+    );
+
+var componentTemplate =
+    $(go.Node, "Vertical",
+        $(go.Shape, "Component", { height: 70, width: 70, fill: "red" },
+            new go.Binding("fill", "color")),
+
+        $(go.TextBlock, { font: "bold 12pt sans-serif" },
+            new go.Binding("text", "key")),
+        $(go.TextBlock, "Description:"),
+        $(go.TextBlock, new go.Binding("text", "desc")),
+
+    );
+
+var packageTemplate =
+    $(go.Node, "Vertical",
+        $(go.Shape, "Package", { height: 70, width: 70, fill: "green" },
+            new go.Binding("fill", "color")),
+
+        $(go.TextBlock, { font: "bold 12pt sans-serif" },
+            new go.Binding("text", "key")),
+        $(go.TextBlock, "Description:"),
+        $(go.TextBlock, new go.Binding("text", "desc")),
+
+    );
+
+// templmap includes all of our templates, we can get access to with "category" inside of an NodeObject. Name of the template is defined in templmap.add("name", variable of Template)
+var templmap = new go.Map();
+templmap.add("", diagram.nodeTemplate);
+templmap.add("Database", databaseTemplate);
+templmap.add("Component", componentTemplate)
+templmap.add("Package", packageTemplate);
+
 
 function init() {
 
     diagram.model = model;
-
-    // default Node template
-    diagram.nodeTemplate = defaultTemplate;
+    // passing our TemplateMap into our diagramS
+    diagram.nodeTemplateMap = templmap;
 
     diagram.layout = $(go.LayeredDigraphLayout);
 
 }
+
