48ace3d96402050cc4aec3393c8509027c042087
b3n3m <benem@web.de>
2021-05-08 18:59:56 +0200

    add Link template draft

--- public/js/index.js
+++ public/js/index.js
@@ -33,6 +33,7 @@ function init() {
     diagram.model = model;
     // passing our TemplateMap into our diagram
     diagram.nodeTemplateMap = templmap;
+    diagram.linkTemplate = linkTemplate;
 
     diagram.layout = $(go.LayeredDigraphLayout);
 
