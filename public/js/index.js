b775585b7c609d92c9087e98638c9e8d984df644
antonroesler <anton.roesler@stud.fra-uas.de>
2021-05-18 20:29:53 +0200

    configured go layout

--- public/js/index.js
+++ public/js/index.js
@@ -35,6 +35,7 @@ function init() {
     // passing our Template Maps into our diagram
     diagram.nodeTemplate = mainTemplate;
     diagram.linkTemplateMap = linkTemplateMap;
+    diagram.layout = $(go.LayeredDigraphLayout);
 }
 
 /**
