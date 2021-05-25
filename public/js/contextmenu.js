4b9f9ac6893fb58914923b986456e7fe558a0dc7
fengyilu <fengyilu1998@gmail.com>
2021-05-25 11:39:18 +0200

    contextmenu update

---
+++ public/js/contextmenu.js
@@ -0,0 +1,26 @@
+diagram.contextMenu =
+    $("ContextMenu",
+        $("ContextMenuButton",
+            $(go.TextBlock, "Undo"),
+            { click: function (e, obj) { e.diagram.commandHandler.undo(); } },
+            new go.Binding("visible", "", function (o) {
+                return o.diagram.commandHandler.canUndo();
+            }).ofObject()),
+        $("ContextMenuButton",
+            $(go.TextBlock, "Redo"),
+            { click: function (e, obj) { e.diagram.commandHandler.redo(); } },
+            new go.Binding("visible", "", function (o) {
+                return o.diagram.commandHandler.canRedo();
+            }).ofObject()),
+        $("ContextMenuButton",
+            $(go.TextBlock, "New Node"),
+            {
+                click: function (e, obj) {
+                    e.diagram.commit(function (d) {
+                        var openModal = document.getElementById("create-modal-btn");
+                        addNodeManager = "DiagramCanvasContextMenu";
+                        openModal.click();
+                    });
+                }
+            }
+        ));
