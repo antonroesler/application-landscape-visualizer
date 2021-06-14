e36d6824c0ffd8905a675de9ecb1e524c6f81273
Anton Rösler <anton.roesler@stud.fra-uas.de>
2021-06-14 11:39:17 +0200

    header comment

--- public/js/heatmap.js
+++ public/js/heatmap.js
@@ -1,4 +1,19 @@
+/*
+* Copyright (c) 2021 Ecore. All rights reserved.
+*
+* University:        Frankfurt University of Applied Sciences
+* Study program:     Engineering Business Information Systems
+* Module:            Advanced Programming 2021
+* Professor:         Prof. Dr. Jung, Prof. Dr. Bremm
+* Date:              21.04.2021
+*
+*/
 
+/**
+ * Contains all functions that are used to generate and render the heatmap
+ *
+ * @author Anton Roesler, Leonard Hußke
+ */
 
 
 
