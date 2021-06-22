aebcc01605a2949a9422e4e8b2d62aeb8b763670
Anton Rösler <anton.roesler@stud.fra-uas.de>
2021-06-22 15:59:24 +0200

    Added avg number of child/parents

--- public/js/statKeyFigures.js
+++ public/js/statKeyFigures.js
@@ -1,4 +1,9 @@
 
+
+
+
+
+/* Total number of Nodes / Links*/
 /**
  * Returns the number of nodes in the NodeDataAarry
  * @returns {number} of Nodes
@@ -31,4 +36,40 @@ function totalNumberOf(part){
         createToast(part+"DataArray is missing", 'fail')
         return 0;
     }
+}
+
+/* Average number of children / parents*/
+
+/**
+ * Returns avg number of child nodes.
+ * @returns {*}
+ */
+function avgNumberOfChildren(){
+    return _avgNumberOf(numberOfChildNodes)
+}
+
+/**
+ * Returns avg number of parent nodes
+ * @returns {*}
+ */
+function avgNumberOfParents(){
+    return _avgNumberOf(numberOfParentNodes)
+}
+
+/**
+ * Returns average of all nodes for a function that returns a numeric value for each node.
+ * @param getter e.g. numberOfParentNodes
+ * @returns {*}
+ * @private
+ */
+function _avgNumberOf(getter){
+    const numbers = [];
+    model.nodeDataArray.forEach(node => {
+        numbers.push(getter(node))
+    })
+    return meanOfArray(numbers);
+}
+
+function meanOfArray(arr){
+    return arr.reduce(function(sum, a) { return sum + a },0)/(arr.length||1);
 }
